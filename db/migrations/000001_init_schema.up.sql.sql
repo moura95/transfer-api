c153b8fe21786c6a0545222cdf7e448b566afaa6
guilherme-moura95 <junior.moura19@hotmail.com>
2024-06-08 00:39:04 +0100

    add columns receivers

--- db/migrations/000001_init_schema.up.sql.sql
+++ db/migrations/000001_init_schema.up.sql.sql
@@ -1,15 +1,16 @@
 SET TIME ZONE 'America/Sao_Paulo';
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-CREATE TABLE IF NOT EXISTS receivers (
-                                       uuid          UUID PRIMARY KEY      DEFAULT uuid_generate_v4(),
-                                       name VARCHAR(255) NOT NULL,
-                                       email VARCHAR(255) NOT NULL UNIQUE,
-                                       tax_id varchar(255) NOT NULL UNIQUE,
-                                       created_at    TIMESTAMP    NOT NULL DEFAULT NOW(),
-                                       update_at    TIMESTAMP    NOT NULL DEFAULT NOW()
+CREATE TABLE receivers (
+     uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+     nome VARCHAR(255) NOT NULL,
+     cpfcnpj VARCHAR(14) NOT NULL,
+     pix_key_type VARCHAR(20) NOT NULL CHECK (pix_key_type IN ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA')),
+     pix_key VARCHAR(140) NOT NULL,
+     email VARCHAR(250) NULL,
+     status VARCHAR(20) NOT NULL CHECK (status IN ('Validado', 'Rascunho'))
 );
-CREATE INDEX idx_receivers_email ON receivers (email);
 
-CREATE INDEX idx_receivers_tax_id ON receivers (tax_id);
-
+CREATE INDEX idx_status ON receivers(status);
+CREATE INDEX idx_pix_key_type ON receivers(pix_key_type);
+CREATE INDEX idx_pix_key ON receivers(pix_key);
