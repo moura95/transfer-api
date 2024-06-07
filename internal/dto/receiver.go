78ac4c28f35f8421617f509c70cdbc6700f5d84e
guilherme-moura95 <junior.moura19@hotmail.com>
2024-06-08 00:39:22 +0100

    create some dtos

--- internal/dto/receiver.go
+++ internal/dto/receiver.go
@@ -1,5 +1,38 @@
 package dto
 
+import "github.com/google/uuid"
+
 type CreateReceiverInputDto struct {
-	Name string `json:"name"`
+	Name       string `json:"name"`
+	Cpf        string `json:"cpf"`
+	PixKeyType string `json:"pix_key_type"`
+	PixKey     string `json:"pix_key"`
+	Email      string `json:"email"`
+	Status     string `json:"status"`
+}
+
+type UpdateReceiverInputDto struct {
+	Name       string `json:"name"`
+	Cpf        string `json:"cpf"`
+	PixKeyType string `json:"pix_key_type"`
+	PixKey     string `json:"pix_key"`
+	Email      string `json:"email"`
+}
+
+type ReceiverOutputDto struct {
+	Uuid       uuid.UUID `json:"uuid"`
+	Name       string    `json:"name"`
+	Cpf        string    `json:"cpf"`
+	PixKeyType string    `json:"pix_key_type"`
+	PixKey     string    `json:"pix_key"`
+	Email      string    `json:"email"`
+	Status     string    `json:"status"`
+}
+
+type ReceiverListOutputDto struct {
+	Receivers []ReceiverOutputDto `json:"receivers"`
+}
+
+type DeleteReceiverInputDto struct {
+	Uuids []uuid.UUID `json:"uuids"`
 }
