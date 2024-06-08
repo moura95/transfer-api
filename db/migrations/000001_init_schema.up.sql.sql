6777f43d75e39e2ea8c3d123b10d0661427ccc7e
guilherme-moura95 <junior.moura19@hotmail.com>
2024-06-08 15:36:56 +0100

    Add unique

--- db/migrations/000001_init_schema.up.sql.sql
+++ db/migrations/000001_init_schema.up.sql.sql
@@ -4,10 +4,10 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 CREATE TABLE receivers (
      uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
      name VARCHAR(255) NOT NULL,
-     cpf_cnpj VARCHAR(14) NOT NULL,
+     cpf_cnpj VARCHAR(14) UNIQUE NOT NULL,
      pix_key_type VARCHAR(20) NOT NULL CHECK (pix_key_type IN ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA')),
      pix_key VARCHAR(140) NOT NULL,
-     email VARCHAR(250) NULL,
+     email VARCHAR(250) UNIQUE NOT NULL,
      status VARCHAR(20) NOT NULL CHECK (status IN ('Validado', 'Rascunho')),
      created_at    TIMESTAMP    NOT NULL DEFAULT NOW(),
      update_at    TIMESTAMP    NOT NULL DEFAULT NOW()
