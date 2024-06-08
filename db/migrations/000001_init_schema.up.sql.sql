4ff0c75e996bd16cd2aedf078ffef176a779a0a8
guilherme-moura95 <junior.moura19@hotmail.com>
2024-06-08 14:09:08 +0100

    update migrations and seed

--- db/migrations/000001_init_schema.up.sql.sql
+++ db/migrations/000001_init_schema.up.sql.sql
@@ -3,12 +3,14 @@ CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 CREATE TABLE receivers (
      uuid UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-     nome VARCHAR(255) NOT NULL,
-     cpfcnpj VARCHAR(14) NOT NULL,
+     name VARCHAR(255) NOT NULL,
+     cpf_cnpj VARCHAR(14) NOT NULL,
      pix_key_type VARCHAR(20) NOT NULL CHECK (pix_key_type IN ('CPF', 'CNPJ', 'EMAIL', 'TELEFONE', 'CHAVE_ALEATORIA')),
      pix_key VARCHAR(140) NOT NULL,
      email VARCHAR(250) NULL,
-     status VARCHAR(20) NOT NULL CHECK (status IN ('Validado', 'Rascunho'))
+     status VARCHAR(20) NOT NULL CHECK (status IN ('Validado', 'Rascunho')),
+     created_at    TIMESTAMP    NOT NULL DEFAULT NOW(),
+     update_at    TIMESTAMP    NOT NULL DEFAULT NOW()
 );
 
 CREATE INDEX idx_status ON receivers(status);
--- db/migrations/000002_seed_schema.up.sql
+++ db/migrations/000002_seed_schema.up.sql
@@ -1,7 +1,11 @@
-INSERT INTO receivers (name, email, tax_id)
-VALUES
-    ('recebedor 1', 'recebedor1@example.com', '12345678901'),
-    ('recebedor 2', 'recebedor2@example.com', '23456789012' ),
-    ('recebedor 3', 'recebedor3@example.com', '34567890123'),
-    ('recebedor 4', 'recebedor4@example.com', '45678901234'),
-    ('recebedor 5', 'recebedor5@example.com', '56789012345');
+INSERT INTO receivers (name, cpf_cnpj, pix_key_type, pix_key, email, status) VALUES
+('João Silva', '12345678901', 'CPF', '12345678901', 'joao.silva@example.com', 'Validado'),
+('Maria Oliveira', '98765432100', 'EMAIL', 'maria.oliveira@example.com', 'maria.oliveira@example.com', 'Rascunho'),
+('Pedro Santos', '11122233344', 'TELEFONE', '+5511999998888', 'pedro.santos@example.com', 'Validado'),
+('Ana Costa', '55566677788', 'CNPJ', '12345678000199', 'ana.costa@example.com', 'Rascunho'),
+('Lucas Lima', '44455566677', 'CHAVE_ALEATORIA', '5a8f9e2a-9eaf-4f6a-a15c-24b5eae1d452', 'lucas.lima@example.com', 'Validado'),
+('Fernanda Souza', '88877766655', 'EMAIL', 'fernanda.souza@example.com', 'fernanda.souza@example.com', 'Rascunho'),
+('Bruno Ferreira', '33344455566', 'CPF', '33344455566', 'bruno.ferreira@example.com', 'Validado'),
+('Juliana Almeida', '22233344455', 'TELEFONE', '+5521999997777', 'juliana.almeida@example.com', 'Rascunho'),
+('Rafael Mendes', '99988877766', 'CNPJ', '98765432000122', 'rafael.mendes@example.com', 'Validado'),
+('Carla Rocha', '11133355577', 'CHAVE_ALEATORIA', '9b1f3c9e-3e35-47b8-9299-5c7a3d1b5c90', 'carla.rocha@example.com', 'Rascunho');
