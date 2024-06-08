689eb7844382b9a32d8614408cb9ad5411c06cda
guilherme-moura95 <junior.moura19@hotmail.com>
2024-06-08 14:10:04 +0100

    list receivers

--- internal/api/controller/receivercontroller/list.go
+++ internal/api/controller/receivercontroller/list.go
@@ -4,8 +4,34 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/moura95/transferapi/internal/dto"
+	"github.com/moura95/transferapi/pkg/errors"
+	httpRes "github.com/moura95/transferapi/pkg/response"
 )
 
 func (r *Receiver) list(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{})
+
+	r.logger.Info("List All Receivers")
+	receivers, err := r.service.List()
+	if err != nil {
+		r.logger.Error(err)
+		c.JSON(http.StatusInternalServerError, httpRes.ErrorResponse(errors.FailedToList("Receivers")))
+		return
+	}
+
+	var response []dto.ReceiverOutputDto
+	for _, receiver := range receivers {
+		response = append(response, dto.ReceiverOutputDto{
+			Uuid:       receiver.Uuid,
+			Name:       receiver.Name,
+			CpfCnpj:    receiver.CpfCnpj,
+			PixKeyType: receiver.PixKeyType,
+			PixKey:     receiver.PixKey,
+			Email:      receiver.Email,
+			Status:     receiver.Status,
+		})
+
+	}
+
+	c.JSON(http.StatusOK, httpRes.SuccessResponse(response))
 }
